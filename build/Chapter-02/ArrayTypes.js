71084e1c9268c364ff800db086dd52354a5ddbdd
José Compadre Junior <jose.compadre@oatsolutions.com.br>
2024-04-10 03:45:06 -0300

    Removing build folder

--- build/Chapter-02/ArrayTypes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const activities = ['Sports', 'Travel', 'Party'];
-for (const activity of activities) {
-    // Use toUpperCase string function in an array
-    console.log(activity.toUpperCase());
-}
--- build/Chapter-02/Functions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Function add
-function sum(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log('Result: ' + num);
-}
-printResult(sum(5, 12));
